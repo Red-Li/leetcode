602136d50d8d1675c20bbeb755b4d8d101e20cb7
Red <cunzhi.li@gmail.com>
2014-12-02 10:53:32 +0800

    add copy list with random pointer

--- src/copy-list-with-ramdom-pointer.hpp
+++ src/copy-list-with-ramdom-pointer.hpp
@@ -9,6 +9,43 @@
 class Solution {
 public:
     RandomListNode *copyRandomList(RandomListNode *head) {
+        if(!head)
+            return head;
+    
+        //create node
+        RandomListNode *p = head;
+        while(p){
+            RandomListNode *q = new RandomListNode(p->label);
+            q->next = p->next;
+            p->next = q;
+            p = q->next;
+        }
         
+        //
+        p = head;
+        while(p){
+            p->next->random = (p->random ? p->random->next : 0);
+            p = p->next->next;
+        }
+        
+        //
+        p = head;
+        head = 0;
+        RandomListNode *cp = 0;
+        while(p){
+            RandomListNode *q = p->next;
+            p->next = p->next->next;
+            p = p->next;
+
+            q->next = 0;
+            if(head)
+                cp->next = q;
+            else
+                head = q;
+            cp = q;
+
+        }
+
+        return head;
     }
 };
